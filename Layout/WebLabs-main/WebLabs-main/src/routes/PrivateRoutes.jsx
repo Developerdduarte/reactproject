44fcd88e465a28083c55c857f153bdb09f83eef7
Igor Duarte <developer.d.duarte@gmail.com>
2025-01-27 09:11:34 -0300

    DeleteLayout

--- Layout/WebLabs-main/WebLabs-main/src/routes/PrivateRoutes.jsx
+++
@@ -1,5 +0,0 @@
-// Controle de rotas onde deverá ter autenticação
-export default function RouteVerification ({ children }) {
-    console.log('Passou pela verificação de rota...');
-    return (  children  );
-}
