30b525c2a2439f692b55cee2cb41a743ce8e5a6d
Igor Duarte <developer.d.duarte@gmail.com>
2025-01-26 22:00:11 -0300

    CommitAllProjects

---
+++ ModuloJavaScript/condicionais.js
@@ -0,0 +1,13 @@
+const valor1 = 1
+const valor2 = 2
+const valor3 = 3
+
+if (valor1 == valor2) {
+    console.log("valores iguais")
+} else {
+    console.log("valores diferentes")
+}
+
+if(valor1 > valor3) {
+    console.log("")
+}
